47fa684bea070fc989443f0c736df2a8cb7b67b3
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2024-05-13 16:15:57 +0100

    Refactored get_scores_for_restaurant

--- scores.py
+++ scores.py
@@ -84,26 +84,12 @@ def get_scores_for_restaurant(restaurant_id):
         "SELECT * FROM scores WHERE restaurant_id = ?", (restaurant_id,)
     )
     scores = cursor.fetchall()
+    column_names = [description[0] for description in cursor.description]
     conn.close()
 
     score_list = []
     for score in scores:
-        score_item = {
-            'id': score[0],
-            'location': score[1],
-            'parking': score[2],
-            'meat': score[3],
-            'roast_potatoes': score[4],
-            'cauliflower_cheese': score[5],
-            'veg': score[6],
-            'ambience': score[7],
-            'customer_service': score[8],
-            'overall_value': score[9],
-            'user_id': score[10],
-            'restaurant_id': score[11],
-            'date': score[12],
-            'price': score[13],
-        }
+        score_item = {column_names[i]: score[i] for i in range(len(score))}
         score_list.append(score_item)
 
     return jsonify({'scores': score_list}), 200
